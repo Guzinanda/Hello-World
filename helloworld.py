807b42712b56981944ba458b1125ddab1d81a062
FERNANDA GUZMAN <gzmanfer@gmail.com>
2019-10-13 23:14:54 -0500

    Hello World of Python

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+""" ______________________________________________________________________________________________
+* DATASHEET
+* @file  		helloworld.py
+* @brief 		Traditional Hello World
+* @details 		
+* @author  		Fernanda Guzmán Gómez
+* @date  		13/10/2019.
+
+* @copyright Copyright (c) 2019 Fernanda Guzmán Gómez. All rights reserved.
+* ______________________________________________________________________________________________ */
+"""
+
+print("Hello World")
